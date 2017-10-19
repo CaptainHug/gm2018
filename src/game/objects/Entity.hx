e570cfa90ac3299b13d4c9988e0e8bba5216d485
CaptainHug <jon_r_huggins@yahoo.co.uk>
2017-10-19 17:56:30 +0100

    Started adding game objects.

---
+++ src/game/objects/Entity.hx
@@ -0,0 +1,24 @@
+package game.objects;
+
+import ui.Sprite;
+
+/**
+ * ...
+ * @author hug
+ */
+class Entity extends Sprite 
+{
+
+	public function new() 
+	{
+		super();
+		
+	}
+	
+	
+	override public function dispose():Void
+	{
+		super.dispose();
+	}
+	
+}
---
+++ src/game/objects/Player.hx
@@ -0,0 +1,22 @@
+package game.objects;
+
+/**
+ * ...
+ * @author hug
+ */
+class Player extends Entity 
+{
+
+	public function new() 
+	{
+		super();
+		
+	}
+	
+	
+	override public function dispose():Void
+	{
+		super.dispose();
+	}
+	
+}
